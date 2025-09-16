afe9f50def68d3ede73a89b1f5b3d1766cdcf267
RobertoAlvarenga18 <132228005+RobertoAlvarenga18@users.noreply.github.com>
2025-09-16 01:36:48 -0300

    Update de urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBase: 'https://api.edaberturas.lat'
+  apiBase: '/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBase: 'https://localhost:7001/'
+  apiBase: 'https://api.edaberturas.lat'
 };
